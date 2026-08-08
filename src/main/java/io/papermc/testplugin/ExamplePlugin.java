b985c0763ac666b4da3d16b4533292aa1fe4d8d7
anivcs <anivcs50@gmail.com>
2026-08-07 19:54:44 -0700

    Created item randomizer.

--- src/main/java/io/papermc/testplugin/ExamplePlugin.java
+++ src/main/java/io/papermc/testplugin/ExamplePlugin.java
@@ -4,10 +4,43 @@ import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
 public class ExamplePlugin extends JavaPlugin implements Listener {
+    final ArrayList<String> validItems = new ArrayList<>(List.of(
+            // Natural Blocks
+            "grass_block", "dirt", "stone", "cobblestone", "oak_log",
+            "birch_log", "spruce_log", "sand", "gravel", "clay",
+
+            // Ores & Minerals
+            "coal", "raw_iron", "raw_copper", "raw_gold", "diamond",
+            "emerald", "redstone", "lapis_lazuli", "amethyst_shard", "flint",
+
+            // Tools & Gear
+            "iron_pickaxe", "diamond_sword", "iron_axe", "iron_shovel", "wooden_hoe",
+            "bucket", "spyglass", "clock", "compass", "shears",
+
+            // Food & Crops
+            "apple", "wheat_seeds", "wheat", "carrot", "potato",
+            "melon_slice", "sweet_berries", "raw_beef", "raw_porkchop", "raw_chicken",
+
+            // Mob Drops & Materials
+            "string", "spider_eye", "rotten_flesh", "bone", "gunpowder",
+            "leather", "feathers", "ink_sac", "slimeball", "sugar_cane"));
+
+    private String getNextItem() {
+        Random random = new Random();
+        int randomIndex = random.nextInt(validItems.size());
+        return validItems.get(randomIndex);
+    }
+
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
@@ -16,5 +49,11 @@ public class ExamplePlugin extends JavaPlugin implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         event.getPlayer().sendMessage(Component.text("Hello, " + event.getPlayer().getName() + "!"));
+    }
+
+    @EventHandler
+    public void onPlayerRespawn(PlayerRespawnEvent event) {
+        String randomItem = this.getNextItem();
+        event.getPlayer().sendMessage(Component.text("You must find " + randomItem + "!"));
     }
 }
