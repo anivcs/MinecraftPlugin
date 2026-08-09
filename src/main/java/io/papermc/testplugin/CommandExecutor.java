9f89afe1b175666ac1f1ccb455fb06968b6be562
anikaven <anikaven17@gmail.com>
2026-08-09 16:26:08 -0700

    Added Command Line Interface for pause, resume, and quit.

--- src/main/java/io/papermc/testplugin/CommandExecutor.java
+++ src/main/java/io/papermc/testplugin/CommandExecutor.java
@@ -1,6 +1,5 @@
 package io.papermc.testplugin;
 
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -13,8 +12,16 @@ public class CommandExecutor implements org.bukkit.command.CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         getLogger().info("Command sent: " + command.getName());
         getLogger().info("Args sent: " + (args == null ? "none" : args[0]));
-        if(command.getName().equalsIgnoreCase("itemrun")) {
-            if (args.length > 0 && args[0].equalsIgnoreCase("start")) {
+        if (!command.getName().equalsIgnoreCase("itemrun")) {
+            return false;
+        }
+
+        if (args.length == 0) {
+            return false;
+        }
+
+        return switch (args[0].toLowerCase()) {
+            case "start" -> {
                 if (sender instanceof Player) {
                     Player player = (Player) sender;
                     player.sendMessage("Success!");
@@ -23,9 +30,43 @@ public class CommandExecutor implements org.bukkit.command.CommandExecutor {
                 } else {
                     getLogger().info("This command must be run by a player!");
                 }
-                return true;
+                yield true;
             }
-        }
-        return false;
+            case "pause" -> {
+                if (sender instanceof Player) {
+                    Player player = (Player) sender;
+                    player.sendMessage("Game Paused.");
+                } else {
+                    getLogger().info("This command must be run by a player!");
+                }
+                yield true;
+            }
+            case "resume" -> {
+                if (sender instanceof Player) {
+                    Player player = (Player) sender;
+                    player.sendMessage("Game Resumed");
+                    getLogger().info("This command must be run by a player!");
+                }
+                yield true;
+            }
+            case "quit" -> {
+                if (sender instanceof Player) {
+                    Player player = (Player) sender;
+                    player.sendMessage("Game Quit.");
+                } else {
+                    getLogger().info("This command must be run by a player!");
+                }
+                yield true;
+            }
+            default -> {
+                sender.sendMessage("Error.");
+                yield false;
+            }
+        };
+
+
+
     }
+
+
 }
